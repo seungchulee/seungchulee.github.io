62b8294c99edc5c68735c23934e92d219ecb2461
SC <inthetelevision@gmail.com>
2020-08-24 19:50:28 +0900

    first article

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "첫 게시글",
+        "excerpt":"시작하게 된 이유 코로나로 인해 재택을 하면서 상대적으로 통근하는 2시간-3시간이 줄어들었음 이 시간을 좀 더 효율적으로 사용하고 싶어서 고민함 (다시 통근하면 어떠할지 고민해야할듯) 어디서 TIL을 주워들어서 이를 해보기로 함 주로 TIL, 공부한 개념, 지식 같은 것들을 기록할듯 어떤 것을 채워나갈지 평소에 Swift 해보고 싶어서 아마 swift를 공부할듯 회사에서 Javascript도 접한...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/first-article/",
+        "teaser": null
+      }]
